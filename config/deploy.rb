7876667c256cd87405ca929a14f484d6258d0f83
David Moles <david.moles@ucop.edu>
2018-12-20 16:01:46 -0800

    Initial deploy script (copy all the files)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,17 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock '~> 3.11.0'
 
-set :application, "mrt-store-config"
-set :repo_url, "git@example.com:cdlib/mrt-store-config.git"
+role_account = 'dpr2store'
+set :ssh_options, user: role_account, forward_agent: true
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :application, 'mrt-store-config'
+set :deploy_to, "/apps/#{role_account}/apps/#{fetch(:application)}"
+set :repo_url, 'git@github.com:cdlib/mrt-store-config.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# Use email address rather than username in revision log, if possible
+git_user_email = `git config user.email`.to_s.strip
+set :local_user, git_user_email.empty? ? fetch(:local_user) : git_user_email
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+end
