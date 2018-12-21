a0fa3e3c5e1eaf4bad1f4e6f86f9bc423d3a4f86
David Moles <david.moles@ucop.edu>
2018-12-21 11:00:08 -0800

    Add task to symlink can-info.txt files

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ set :repo_url, 'git@github.com:cdlib/mrt-store-config.git'
 git_user_email = `git config user.email`.to_s.strip
 set :local_user, git_user_email.empty? ? fetch(:local_user) : git_user_email
 
+env = fetch(:stage) # yes, 'stage' means 'environment' to Capistrano
+
 namespace :git do
   desc 'Prompt for tag'
   task :prompt_for_tag do
@@ -27,7 +29,6 @@ end
 namespace :deploy do
   desc 'Generate store-info.txt from ERB template'
   task :gen_store_info do
-    env = fetch(:stage) # yes, 'stage' means 'environment' to Capistrano
     on roles(:all) do |server|
       within "#{release_path}/config/src/#{env}/store" do
         # Use the deployed ERB, not the local copy
@@ -46,22 +47,28 @@ namespace :deploy do
   namespace :symlink do
     desc 'Symlink nodes.txt and store-info.txt into mrtHomes directory'
     task :mrt_homes do
-      env = fetch(:stage) # yes, 'stage' means 'environment' to Capistrano
-      deployed_store = Pathname.new("#{current_path}/config/src/#{env}/store")
-      mrt_homes_store = Pathname.new("/apps/#{role_account}/mrtHomes/store")
+      mrt_homes_store_dir = Pathname.new("/apps/#{role_account}/mrtHomes/store")
+      deployed_store_dir = Pathname.new("#{current_path}/config/src/#{env}/store")
       on roles(:all) do |_|
-        within mrt_homes_store do
+        within mrt_homes_store_dir do
           %w(nodes.txt store-info.txt).each do |filename|
+            info "Checking #{filename} symlink"
+
+            # See if symlink already exists
             if test('[', '-L', filename, ']')
               info "#{filename} already symlinked"
               next
             end
+
+            # Move existing file, if present
             if test('[', '-f', filename, ']')
               new_name = "#{filename}.#{Time.now.to_i}"
               warn "#{filename} exists, but is a plain file; renaming to #{new_name}"
               execute(:mv, filename, new_name)
             end
-            deployed_file_path = deployed_store + filename
+
+            # Create symlink
+            deployed_file_path = deployed_store_dir + filename
             info "Creating symlink to #{deployed_file_path}"
             execute(:ln, '-s', deployed_file_path, filename)
           end
@@ -69,10 +76,57 @@ namespace :deploy do
       end
     end
 
-    # TODO: symlink individual nodes
+    desc 'Symlink can-info.txt files for individual nodes'
+    task :can_info do
+      repo_dir = Pathname.new("/apps/#{role_account}/repository")
+      deployed_repo_dir = Pathname.new("#{current_path}/config/src/#{env}/repository")
+      deployed_store_dir = Pathname.new("#{current_path}/config/src/#{env}/store")
+      on roles(:all) do |_|
+        # Use the deployed nodes.txt, not the local copy
+        nodes_txt = download!(deployed_store_dir + 'nodes.txt')
+        known_nodes = nodes_txt.gsub("#{repo_dir}/", '').split("\n")
+        within(repo_dir) do
+          known_nodes.each do |node|
+            info "Checking can-info.txt symlink for #{node}"
+
+            # Ensure node directory exists
+            unless test('[', '-d', node, ']')
+              info "Node directory #{repo_dir}/#{node} does not exist; creating"
+              execute :mkdir, node
+            end
+
+            # See if symlink already exists
+            can_info = Pathname.new(node) + 'can-info.txt'
+            if test('[', '-L', can_info, ']')
+              info "#{can_info} already symlinked"
+              next
+            end
+
+            # Move existing file, if present
+            if test('[', '-f', can_info, ']')
+              new_name = "#{can_info}.#{Time.now.to_i}"
+              warn "#{can_info} exists, but is a plain file; renaming to #{new_name}"
+              execute(:mv, can_info, new_name)
+            end
+
+            # Create symlink
+            deployed_can_info = deployed_repo_dir + can_info
+            info "Creating symlink to #{deployed_can_info}"
+            execute(:ln, '-s', deployed_can_info, can_info)
+          end
+
+          # See what extra node directories might be hanging around
+          all_node_dirs = (capture(:ls, '-d', 'node*')).split
+          (all_node_dirs - known_nodes).each do |unknown_node|
+            warn "Unused node directory #{unknown_node}"
+          end
+        end
+      end
+    end
   end
 end
 
 before 'deploy', 'git:prompt_for_tag'
 after 'deploy:set_current_revision', 'deploy:gen_store_info'
 after 'deploy:symlink:release', 'deploy:symlink:mrt_homes'
+after 'deploy:symlink:release', 'deploy:symlink:can_info'
